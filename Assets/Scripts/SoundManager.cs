27ca5d19f450ea9c951ef49a994decfec3cbfbc4
xiu0922koway <3160104946@zju.edu.cn>
2025-01-26 15:41:57 +0200

    add sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    // Start is called before the first frame update
+    public List<AudioClip> audioClips;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClip(int index, float volumn)
+    {
+        this.GetComponent<AudioSource>().PlayOneShot(audioClips[index], volumn);
+    }
+}
